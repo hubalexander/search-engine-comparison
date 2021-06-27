be97ae57c8414fc7509c041ead979a7d6413ec6d
hubalexander <har14ape@student.lu.se>
2021-06-27 10:00:02 +0200

    Added empty keys

---
+++ keys/keys.java
@@ -0,0 +1,12 @@
+package keys;
+
+public enum keys {
+    KEYDATA;
+
+    // Google keys
+    public String GOOGLE_API_KEY = "";
+    public String GOOGLE_ENGINE_ID = "";
+
+    // Bing keys
+    public String BING_KEY = "";
+}
